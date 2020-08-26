e064e913c033a3b169b06524bbf9077c47be96d1
eliteSCHW31N <admin@elitesch31n.de>
2020-08-26 14:56:41 +0200

    add Main Config add Bot

--- src/main/java/de/eliteschw31n/Main.java
+++ src/main/java/de/eliteschw31n/Main.java
@@ -1,11 +1,62 @@
 package de.eliteschw31n;
 
 
+import de.eliteschw31n.utils.MainConfiguration;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
+
+import javax.security.auth.login.LoginException;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 public class Main {
     public static Properties properties = new Properties();
+    public MainConfiguration mainConfiguration;
+    public JDA discordBot;
+
+    public Main() {
+        System.out.println("PrinterCord " + properties.getProperty("version") + " starting...");
+        System.out.println("Check Local Files...");
+        final DumperOptions options = new DumperOptions();
+        options.setIndent(2);
+        options.setPrettyFlow(true);
+        options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+        Yaml yaml = new Yaml(options);
+        File mainConfigurationFile = new File("mainConfig.yaml");
+        if (!mainConfigurationFile.exists()) {
+            System.out.println("Main Configuration not found! Generating it now!");
+            try {
+                Map<String, Object> data = new HashMap<>();
+                data.put("discordToken", "123456789");
+                mainConfigurationFile.createNewFile();
+                FileWriter writer = new FileWriter(mainConfigurationFile.getAbsoluteFile());
+                yaml.dump(data, writer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+        yaml = new Yaml();
+        InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("mainConfig.yaml");
+        Map<String, Object> yamlData = yaml.load(inputStream);
+        mainConfiguration = new MainConfiguration(((String) yamlData.get("discordToken")));
+        System.out.println("Check Database...");
+        System.out.println("Start Bot...");
+        try {
+            discordBot = JDABuilder.createDefault(mainConfiguration.getDiscordToken()).build();
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Load Instances...");
+        System.out.println("Startup Done!");
+    }
 
     public static void main(String[] args) {
         try {
@@ -13,10 +64,6 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("PrinterCord " + properties.getProperty("version") + " starting...");
-        System.out.println("Check Local Files...");
-        System.out.println("Check Database...");
-        System.out.println("Load Instances...");
-        System.out.println("Startup Done!");
+        new Main();
     }
 }
---
+++ src/main/java/de/eliteschw31n/utils/MainConfiguration.java
@@ -0,0 +1,13 @@
+package de.eliteschw31n.utils;
+
+public class MainConfiguration {
+    final String discordToken;
+
+    public MainConfiguration(final String discordToken) {
+        this.discordToken = discordToken;
+    }
+
+    public String getDiscordToken() {
+        return discordToken;
+    }
+}
